92499545be7444354b392bd9c8836227da2a7579
LSLNG <lawrencesuilunng@gmail.com>
2018-10-07 15:00:28 -0700

    alert on submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Header from './components/header';
 import Footer from './components/footer';
 import Carousel from './components/carousel';
+import LoginForm from './components/loginForm';
 import './App.css';
 
 class App extends Component {
+  // pass in initial states
+  constructor(){
+    super();
+    this.state = {
+      loggedIn: false,
+      accountCreated: false
+    }
+  }
 
+  buttonClick = (e) => {
+    e.preventDefault();
+    alert("submit");
 
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
         <Carousel />
-         
+        <LoginForm buttonClick={this.buttonClick}/>
         <Footer />
       </div>
     );
--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -1,33 +1,33 @@
 import React from "react";
 
-const loginForm = () => (
-    <form>
-    <div class="form-group row">
-        <div class="col-7">
-            <label for="username"><b>Username</b></label>
-            <input id="username" type="text" placeholder="Enter Username" name="username" required>
-            <br>
-            <label for="first_name"><b>First Name</b></label>
-            <input id="firstName" type="text" placeholder="Enter your first name" name="first_name" required>
-            <br>
-            <label for="last_name"><b>Last Name</b></label>
-            <input id="lastName" type="text" placeholder="Enter your last name" name="last_name" required>
-            <br>
-            <label for="email"><b>Email</b></label>
-            <input id="email" type="email" placeholder="Enter Email" name="email" required>
-            <br>
-            <label for="password"><b>Password</b></label>
-            <input id="pw" class="pw" type="password" placeholder="Enter Password" name="password" required>
-            <br>
-            <label for="password_reenter"><b>Re-Enter Password</b></label>
-            <input id="repw"  class="repw" type="password" placeholder="Enter Password" name="password_reenter"         required>
-            <label class="d-none" for="profileAvatar"><b>profileAvatar</b></label>
-            <input id="av" class="av d-none" type="text" placeholder="profileAvatarPath" name="profileAvatar"       style="width: 250px;"></span> 
-            <hr>
-            <span class="font-weight-light">Password must be between 8 - 16 characters in length</span>
-            <br><br>
-            <button id="createAccount" type="submit" class="btn btn-primary">Create Account!</button>
-        </div>
-        </form>
-);
+const LoginForm = (props) => (
+    <form onSubmit={props.buttonClick}>
+            <div className="form-group row">
+    <div className="col-7">
+        <label for="username"><b>Username</b></label>
+        <input id="username" type="text" placeholder="Enter Username" name="username" required />
+        <br />
+        <label for="first_name"><b>First Name</b></label>
+        <input id="firstName" type="text" placeholder="Enter your first name" name="first_name" required />
+        <br />
+        <label for="last_name"><b>Last Name</b></label>
+        <input id="lastName" type="text" placeholder="Enter your last name" name="last_name" required />
+        <br />
+        <label for="email"><b>Email</b></label>
+        <input id="email" type="email" placeholder="Enter Email" name="email" required />
+        <br />
+        <label for="password"><b>Password</b></label>
+        <input id="pw" className="pw" type="password" placeholder="Enter Password" name="password" required />
+        <br />
+        <label for="password_reenter"><b>Re-Enter Password</b></label>
+        <input id="repw"  className="repw" type="password" placeholder="Enter Password" name="password_reenter" required />
+        <hr />
+        <span className="font-weight-light">Password must be between 8 - 16 characters in length</span>
+        <br /><br />
+        <button id="createAccount" type="submit" className="btn btn-primary">Create Account!</button>
+    </div>
+    </div>
+    </form>
+)
+
 export default LoginForm;
