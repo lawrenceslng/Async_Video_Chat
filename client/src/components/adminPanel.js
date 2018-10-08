1f4a7c7cd09d4783657badb820c38b5975fed558
Kelly Hook <kellymhook@gmail.com>
2018-10-07 20:22:23 -0700

    finished adminPanel page

--- client/src/components/adminPanel.js
+++ client/src/components/adminPanel.js
@@ -1,7 +1,60 @@
 import React from "react";
 
 const adminPanel = () => (
-//code will go here
-)
+  <div class="box">
+    <div class="container">
+     	<div class="row">
 
+      <div class="col-sm-6 ">
+       <a href="#"/>
+       <div class="box-part text-center">
+         <div class="title">
+           <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+         </div>
+         <div class="text">
+              <span><h2>Thought Archives</h2></span>
+         </div>
+        </div>
+     </div>
+
+				 <div class="col-sm-6 ">
+					<a href="#"/>
+					<div class="box-part text-center">
+						<div class="title">
+							<img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+						</div>
+						<div class="text">
+                <span><h2>Incoming Requests</h2></span>
+						</div>
+					 </div>
+				</div>
+
+        <div class="col-sm-6 ">
+         <a href="#"/>
+         <div class="box-part text-center">
+           <div class="title">
+             <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+           </div>
+           <div class="text">
+               <span><h2>Create New</h2></span>
+           </div>
+          </div>
+       </div>
+
+       <div class="col-sm-6 ">
+        <a href="#"/>
+        <div class="box-part text-center">
+          <div class="title">
+            <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+          </div>
+          <div class="text">
+               <span><h2>Your Community</h2></span>
+          </div>
+         </div>
+      </div>
+
+		</div>
+    </div>
+</div>
+)
 export default adminPanel;
