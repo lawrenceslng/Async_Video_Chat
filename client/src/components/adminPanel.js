2a550a59fa456306e4826284f1412c6c810cfecc
Kelly Hook <kellymhook@gmail.com>
2018-10-07 20:37:02 -0700

    added temp images

--- client/src/components/adminPanel.js
+++ client/src/components/adminPanel.js
@@ -9,7 +9,7 @@ const adminPanel = () => (
        <a href="#"/>
        <div class="box-part text-center">
          <div class="title">
-           <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+           <img class="card-img-top" src="https://visualpharm.com/assets/224/Folder-595b40b85ba036ed117dd27b.svg" alt=" image"/>
          </div>
          <div class="text">
               <span><h2>Thought Archives</h2></span>
@@ -21,7 +21,7 @@ const adminPanel = () => (
 					<a href="#"/>
 					<div class="box-part text-center">
 						<div class="title">
-							<img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+							<img class="card-img-top" src="https://visualpharm.com/assets/168/Read%20Message-595b40b75ba036ed117d88f5.svg" alt=" image"/>
 						</div>
 						<div class="text">
                 <span><h2>Incoming Requests</h2></span>
@@ -33,7 +33,7 @@ const adminPanel = () => (
          <a href="#"/>
          <div class="box-part text-center">
            <div class="title">
-             <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+             <img class="card-img-top" src="https://visualpharm.com/assets/375/Create-595b40b75ba036ed117d7bbf.svg" alt=" image"/>
            </div>
            <div class="text">
                <span><h2>Create New</h2></span>
@@ -45,7 +45,7 @@ const adminPanel = () => (
         <a href="#"/>
         <div class="box-part text-center">
           <div class="title">
-            <img class="card-img-top" src="img_avatar1.png" alt=" image"/>
+            <img class="card-img-top" src="https://static.thenounproject.com/png/5040-200.png" alt=" image"/>
           </div>
           <div class="text">
                <span><h2>Your Community</h2></span>
