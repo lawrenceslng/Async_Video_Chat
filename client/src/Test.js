15f33e3b235a1fc1d9ec20a96263e25f69867230
LSLNG <lawrencesuilunng@gmail.com>
2018-10-11 21:16:06 -0700

    test environment

---
+++ client/src/Test.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import Header from './components/header';
+// import Footer from './components/footer';
+// import Carousel from './components/carousel';
+// import LoginForm from './components/loginForm';
+// import PWMatch from './components/accountCreate';
+// import AdminPanel from './components/adminPanel';
+
+import './App.css';
+
+class Test extends Component {
+  // pass in initial states
+  constructor(){
+   
+  }
+
+  render() {
+
+  }
+}
+
+export default Test;
