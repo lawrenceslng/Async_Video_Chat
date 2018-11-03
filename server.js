23635e363ef4bca81f99fcf40632f5a6a4a9a38d
LSLNG <lawrencesuilunng@gmail.com>
2018-11-02 19:33:22 -0700

    adding internet

--- server.js
+++ server.js
@@ -63,7 +63,16 @@ var connection = mysql.createConnection({
     database: process.env.DB_NAME    //TBD
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // app.get('/', function(req, res){
 // 	res.send('hi');
 // });
